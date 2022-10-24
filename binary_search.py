9b31ae6e5d2099df2c5ad33d8d98e82862f8c6b0
Nayeli Pleskac <pleskac510@gmail.com>
2022-10-23 19:04:58 -0700

    comparing timeit v stopwatch

--- binary_search.py
+++ binary_search.py
@@ -3,7 +3,13 @@
 #assuming the list is sorted ..
 #set 3
 #iterative
-import random, time
+import random, time, timeit
+
+##########################################
+## iterative binary search
+## binary search algorithm with while true
+##########################################
+
 # nums = [3,6,13,8,23,21]
 
 # nums.sort()
@@ -28,8 +34,11 @@ import random, time
 #     elif x < nums[mid_index]:
 #         max_index = mid_index -1
 
+####################################################
+## recursive binary search
+## binary search algorithm with recursion and timeit
+####################################################
 
-#recursive binary search
 def binarySearch(nums, x, min_index, max_index):
     if max_index>=min_index:
         mid_index = (min_index + max_index) // 2
@@ -40,12 +49,10 @@ def binarySearch(nums, x, min_index, max_index):
             return mid_index 
 
         elif x > nums[mid_index]: #x greater than median
-            # min_index = mid_index +1
             return binarySearch(nums, x, mid_index+1, max_index)
 
         elif x < nums[mid_index]: #x smaller than median
             return binarySearch(nums,x,min_index, mid_index-1)
-            # max_index = mid_index -1
         
 
 nums = [3,6,13,8,23,21]
@@ -53,6 +60,10 @@ nums.sort()
 x= random.choice(nums)
 min_index = 0  
 max_index = len(nums)-1
-binarySearch(nums, x, min_index, max_index)
+# binarySearch(nums, x, min_index, max_index)
+
+t= timeit.timeit(stmt=lambda: binarySearch(nums,x,min_index,max_index), number = 1)
+t = round(t,8)
+print('exeuction time using timeit : {:.8f} sec\n'.format(t))
 
 
--- recursive.py
+++ recursive.py
@@ -42,26 +42,26 @@ def linearSearch(nums):
 # print('The time difference is : ', timeit.default_timer() - start_time)
 
 #####################################################################
-## second linear search 
+## second linear search - conclusion: timeit more efficient -
 ## linear search algorithm uisng runtime calculations (timeit.timeit)
 #####################################################################
 
-nums = [2,4,6,8,10,12,14]
-# num_var = random.choice(nums)
+# nums = [2,4,6,8,10,12,14]
+# # num_var = random.choice(nums)
 
-def linearSearch(nums, num): #set of nums, target
-    for i in nums:
-        if i == num:
-            print('found the number:',i, ' index: ',nums.index(i))
-            break
-        else:
-            print('number not found:',i, ' index:', nums.index(i))
+# def timeitLinearSearch(nums, num): #set of nums, target
+#     for i in nums:
+#         if i == num:
+#             # print('found the number:',i, ' index: ',nums.index(i))
+#             break
+#         # else:
+#         #     print('number not found:',i, ' index:', nums.index(i))
 
-# t = timeit.Timer(stmt=lambda: linearSearch(nums), number = 1).timeit()
-for i in nums:
-    t = timeit.timeit(stmt=lambda: linearSearch(nums, i), number = 1)
-    # t=round(t,8)
-    print('runtime for {}: {} secs\n'.format(i,t))
+# # t = timeit.Timer(stmt=lambda: linearSearch(nums), number = 1).timeit()
+# for i in nums:
+#     t = timeit.timeit(stmt=lambda: timeitLinearSearch(nums, i), number = 1)
+#     t=round(t,8)
+#     print('runtime for {}: {:.8f} secs using TIMEIT\n'.format(i,t))
 
 ###########################################################################
 ## third linear search 
@@ -70,24 +70,28 @@ for i in nums:
 
 # nums = [2,4,6,8,10,12,14]
 
-# def linearSearch(nums, num): #set of nums, target
+# def stopWatchLinearSearch(nums, num): #set of nums, target
 #     stopwatch = Stopwatch()
 #     stopwatch.start()
 #     # t= stopwatch.
 #     # t.start()
 #     for i in nums:
 #         if i == num:
-#             print('found the number:',i, ' index: ',nums.index(i))
+#             # print('found the number:',i, ' index: ',nums.index(i))
 #             stopwatch.stop()
-#             print('time elapsed for {}: {} secs\n '.format(i, stopwatch.duration))
+#             print('time elapsed for {}: {:.8f} secs using STOPWATCH \n '.format(i, round(stopwatch.duration,8)))
 #             break
 #         # else:
 #         #     print('number not found:',i, ' index:', nums.index(i))
         
 # for i in nums:
-#     linearSearch(nums,i)
+#     stopWatchLinearSearch(nums,i)
 
-# start = time.time()
+#######################################
+## recursive excersise 1
+## recursive example to find factorials
+#######################################
+
 
 # def factorial(n):
 #     if n is 1: #This is the base case
@@ -95,10 +99,16 @@ for i in nums:
 #     return n * factorial(n-1)
 # n = int(input("Number? "))
 # print(factorial(n))
-# end = time.time()
-# print('The time of execution of the linear search using time.time is: ', (end - start), 'ms') 
 
-#exercise 2
+# t = timeit.timeit(stmt=lambda: factorial(n), number = 1)
+# t=round(t,8)
+# print('runtime for factorial using timeit : {:.8f} secs'.format(t)) 
+
+###########################################################
+## recursive excersise 2
+## recusive example to find fibonacci sequence using timeit
+###########################################################
+
 # def fibonacci(n):
 #     if n ==0:
 #         return 0
@@ -108,12 +118,24 @@ for i in nums:
 #         return fibonacci(n-1) + fibonacci(n-2)
 # n = int(input('Find the nth number in fibonacci sequence: ')) 
 # #print just n
-# print(fibonacci(n)) 
-# #to print out sequence of n
+# print('number in sequence : ',fibonacci(n)) 
+# t = timeit.timeit(stmt=lambda: fibonacci(n), number = 1)
+# t= round(t,8)
+# print('runtime for program : {:.8f} secs using timeit\n'.format(t))
+
+#to print out sequence of n
 # for i in range(n):
 #     print(fibonacci(i))
+    # t = timeit.timeit(stmt=lambda: fibonacci(n), number = 1)
+    # t=round(t,8)
+    # print('runtime for {} : {:.8f} secs using timeit\n'.format(x,t))
+   
 
-#exercise 3
+#######################################################
+## recursive excersise 3
+## recursive formula to find sums of digits with timeit
+#######################################################
+
 # print(123%10+(123//10))
 
 #adding the sum of entered digits
@@ -125,10 +147,18 @@ for i in nums:
 #         return n%10+sum(n//10)
 
 # n = int(input('Enter a number: '))
+
 # print('sum: ', sum(n))
+# t= timeit.timeit(stmt=lambda: sum(n), number = 1)
+# t = round(t,8)
+# print('time of program : {:.8f} sec\n'.format(t))
 
 
-#exercise 4
+#########################################
+## recursive excersise 4
+## recursive formula to reverse a string
+#########################################
+
 # print('hello'[1:])
 # def reverse(x):
 #     if len(x) == 0:
@@ -138,8 +168,16 @@ for i in nums:
 
 # x = input('Enter a string: ')
 # print(reverse(x))
+# t= timeit.timeit(stmt=lambda: reverse(x), number = 1)
+# t = round(t,8)
+# print('time of program : {:.8f} sec\n'.format(t))
 
-#exercise 5- pascal's triangle without recursion
+######################################
+## recursive excersise 5
+## pascal's triangle without recursion
+######################################
+
+
 # print(1)
 # layer = [1, 1]
 # layers = 1
@@ -156,36 +194,34 @@ for i in nums:
 #     layer=new_layer
 #     layers+=1
 
-#with recursion
+##############################################
+## recursive excersise 5
+## pascal's triangle with recursion and timeit
+##############################################
 
-# def pascal(layer): 
-#     # layer = [1,1]
-#     # print('layers: ', layers)
-#     if len(layer) > 10:  #base cases
-#         return
-#     # if layers == 1:
-#     #     return [[1]]   
-#     else: 
-#         for i in layer:
-#             print(i, end = ' ')
-#         print()
-#         #recursive call :
-#         # rec_row = pascal(layer-1)
-#         # prev_layer= rec_row[-1]
-#         new_layer = []
-#         for i in range(len(layer)-1):
-#             num1= layer[i]
-#             num2 = layer[i+1]
-#             new_layer.append(num1 + num2)
+def pascal(layer): 
+    if len(layer) > 10:  #base cases
+        return
+    else: 
+        for i in layer:
+            print(i, end = ' ')
+        print()
+        new_layer = []
+        for i in range(len(layer)-1):
+            num1= layer[i]
+            num2 = layer[i+1]
+            new_layer.append(num1 + num2)
 
-#         new_layer = [1] + new_layer + [1] 
-#         layer = new_layer
-#         # rec_row.append(new_layer)     
-#         pascal(layer)
+        new_layer = [1] + new_layer + [1] 
+        layer = new_layer
+        pascal(layer)
         
-# print(1)
-# layer = [1,1]
+print(1)
+layer = [1,1]
 # pascal(layer)
+t= timeit.timeit(stmt=lambda: pascal(layer), number = 1)
+t = round(t,8)
+print('time of program : {:.8f} sec\n'.format(t))
 
 
 
