8fe1df4cc6dd49eac1b245f9d905173826543be9
Nayeli Pleskac <pleskac510@gmail.com>
2022-11-01 20:12:34 -0700

    bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,14 +1,33 @@
 #Bubble Sort
 
-import time, random
+import timeit, random
 
-#########
-## time complexity: 
-## runtime: 
-## 
+#############################
+## bubble sort algorithm
+## runtime & time complexity:
+## O(n^2)
+############################# 
+
+def bubbleSort(nums, n):
+    swapped = False
+    for a in range(0,n-1):
+        for b in range(0,n-1):
+            if nums[b] > nums[b+1]:
+                swapped = True
+                nums[b],nums[b+1] = nums[b+1], nums[b]
+    return nums
 
 nums = [5,18,22,13,70,1,7]
-flagged = False
-for i in nums:
-    print(i)
+numsLength= len(nums)
+# swapped = False
+
+print('unsorted list: ', nums)
+print('sorted list: ', bubbleSort(nums, numsLength))
+
+t = timeit.timeit(stmt=lambda: bubbleSort(nums, numsLength), number = 1)
+t=round(t,8)
+print('runtime for program: {:.8f} secs using TIMEIT\n'.format(t))
+
+
+
 
