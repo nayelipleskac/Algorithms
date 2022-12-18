37d97088b4ac87cc4228646b4070706391f7f869
Nayeli Pleskac <pleskac510@gmail.com>
2022-12-18 09:05:22 -0800

    client server

--- sockets/client.py
+++ sockets/client.py
@@ -1,14 +1,15 @@
 import socket
 
-host = '127.0.0.1'
-port = 12345
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '127.0.0.1' #localhost
+port = 1234
 
 s.connect((host, port)) #connect to host and port of server
 print('connected')
 
-while True: 
-    data = s.recv(1024)
-    print(data.decode())
-    data = input('Client: ')
-    s.sendall(data.encode())
+data = s.recv(1024)
+print(data)
+s.close()
+# print(data.decode())
+# data = input('Client: ')
+# s.sendall(data.encode())
--- sockets/server.py
+++ sockets/server.py
@@ -1,25 +1,29 @@
-import socket
+import socket, atexit
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Address Family (AF) - IPv4
 #Socket Type- TCP
 host = '127.0.0.1' #binds to localhost 127.0.0.1
-port = 12345
+port = 1234
 
-#.bind() method is used to associate the socket 
-#with a specific network interface and port #
-# .listen() enables a server to accept connections
-# .accept() method blocks execution and waits for an incoming connection
+def handle_exit():
+    print('This runs after a keyboard interrupt')
+    s.close()
+atexit.register(handle_exit)
 
 s.bind((host,port))
+print('socket binded to port %s' %(port))
 s.listen()
-print('Socket is listening')
-conn, addr = s.accept() #get client's socket obj and network address
+print('Socket is listening...')
 
-with conn:
+while True: 
+    conn, addr = s.accept() #get client's socket obj and network address
     print('Got a connecton from ', addr)
-    while True: 
-        data = input('Server: ')
-        conn.sendall(data.encode())
-        data = conn.recv(1024)
-        print(addr,":", data.decode())
+    conn.send('thank you for connecting '.encode())
+    s.close()
+    break
+    # data = input('Server: ')
+    # conn.sendall(data.encode())
+    # data = conn.recv(1024)
+    # print(addr,":", data.decode())
+    
