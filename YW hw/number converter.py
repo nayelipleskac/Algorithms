802db984010122ec34e451b897987079f8d5c015
Nayeli Pleskac <pleskac510@gmail.com>
2023-02-11 11:56:57 -0800

    YW hw

--- YW hw/number converter.py
+++ YW hw/number converter.py
@@ -1,3 +1,41 @@
-numInput = (input('Enter a number: '))
-newWord = '*' * len(numInput)
-print(newWord)
+# def check(number,numLength):
+#     for x in numLength:
+#         if x == '.':
+#             return
+#         else:
+#             x = '*'
+#     print(number)
+
+# numInput = (input('Enter a number: '))
+# check(numInput,len(numInput))
+
+#astericks excersise
+ 
+# numInput = (input('Enter a number: '))
+# newWord = '*' * len(numInput)
+# print(newWord)
+
+#random nums
+
+# import random
+# x = random.randint(0,500)
+# y = random.randint(0,500)
+# z = random.randint(0,500)
+
+# print(x,y,z)
+# if x > y and x>z:
+#     print('The largest is: ', x)
+# if y > z and y>x:
+#     print('The largest is: ', y)
+# if z > x and z> y:
+#     print('The largest is: ', z)
+
+# shapes={'square':4, 'circle':0, 'hexagon':6, 'decagon':10}
+
+# userShape = input('What is your favorite shape? ')
+# if userShape in shapes:
+#     print(userShape, shapes[userShape])
+# else: 
+#     sides = input('How many sides in this shape? ')
+#     shapes[userShape] = sides
+#     print(shapes)
